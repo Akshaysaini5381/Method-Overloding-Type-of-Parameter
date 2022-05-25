bbfb7669e0ab9bc5ebd9bf483df7cbd7bf5e7f63
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-25 21:11:09 +0530

    Add project files.

---
+++ Method Overloding  Type of Parameter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloding__Type_of_Parameter
+{
+
+    class name
+    {
+        public void show(int num, int num1)
+        {
+            Console.WriteLine(" Total number :" + (num + num1));
+        }
+
+        public void show(string Name, string Name1)
+        {
+            Console.WriteLine(" string :" + Name +" "+ Name1);
+        }
+        public void show(float x, double y)
+        {
+            Console.WriteLine("total :" + (x + y));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            name obj = new name();
+            obj.show("hello", "hii");
+
+            Console.ReadLine();
+        }
+    }
+}
